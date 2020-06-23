ab8c4507a598d004cc72ba1551b04c68f563f9eb
Razaul Hasan Ansari <razaulhasanansari2000@gmail.com>
2020-06-23 13:33:04 +0530

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return{
@@ -29,8 +30,9 @@ class About extends Component {
             );
         }
 
-        return(
-            <ScrollView>
+        if(this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
                 <Card title="Our History">
                 <Text>
                 Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong.
@@ -42,14 +44,56 @@ class About extends Component {
                 </Text>
             </Card>
             <Card title="Corporate Leadership">
-            <FlatList
-                data={this.props.leaders.leaders}
-                renderItem={renderLeaders}
-                keyExtractor={item => item.id.toString()}
-                />
+                <Loading />
             </Card>
             </ScrollView>
-        );
+            );
+        }
+
+        else if(this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                <Card title="Our History">
+                <Text>
+                Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong.
+                With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list 
+                clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will 
+                arrive on your plate the next time you visit us.
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan,
+                that featured for the first time the world's best cuisines in a pan.
+                </Text>
+            </Card>
+            <Card title="Corporate Leadership">
+                <Text>{this.props.leaders.errMess}</Text>
+            </Card>
+            </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <Card title="Our History">
+                    <Text>
+                    Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong.
+                    With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list 
+                    clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will 
+                    arrive on your plate the next time you visit us.
+                    The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan,
+                    that featured for the first time the world's best cuisines in a pan.
+                    </Text>
+                </Card>
+                <Card title="Corporate Leadership">
+                <FlatList
+                    data={this.props.leaders.leaders}
+                    renderItem={renderLeaders}
+                    keyExtractor={item => item.id.toString()}
+                    />
+                </Card>
+                </ScrollView>
+            );
+        }
+
+        
     }
     
 }
